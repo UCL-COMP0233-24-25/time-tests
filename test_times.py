93880823193796823e99f74ad40fba03613d18d1
Jiasheng Yang <jy445@nau.edu>
2024-10-22 11:55:01 +0100

    add the unit test to times.py and fix the bug

---
+++ test_times.py
@@ -0,0 +1,12 @@
+import pytest
+from times import time_range, compute_overlap_time
+
+def test_given_input():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", number_of_intervals=1, gap_between_intervals_s=0)
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", number_of_intervals=2, gap_between_intervals_s=60)
+
+    result = compute_overlap_time(large, short)
+    expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+
+    # judge that the result matches the expected value
+    assert result == expected, f"Expected: {expected}, but Result: {result}, doesn't match!"
--- times.py
+++ times.py
@@ -4,7 +4,10 @@ import datetime
 def time_range(start_time, end_time, number_of_intervals=1, gap_between_intervals_s=0):
     start_time_s = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
     end_time_s = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
+    # I think that it is easier to understand use my below equation:
+    # d = ((end_time_s - start_time_s).total_seconds() - gap_between_intervals_s * (number_of_intervals - 1)) / number_of_intervals
     d = (end_time_s - start_time_s).total_seconds() / number_of_intervals + gap_between_intervals_s * (1 / number_of_intervals - 1)
+    # sec_range = [(start1, end1), (start2, end2)]
     sec_range = [(start_time_s + datetime.timedelta(seconds=i * d + i * gap_between_intervals_s),
                   start_time_s + datetime.timedelta(seconds=(i + 1) * d + i * gap_between_intervals_s))
                  for i in range(number_of_intervals)]
@@ -17,10 +20,24 @@ def compute_overlap_time(range1, range2):
         for start2, end2 in range2:
             low = max(start1, start2)
             high = min(end1, end2)
-            overlap_time.append((low, high))
+            # BUG there is a bug for the original function, it might generate an invalid overlap time, for example
+            # large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+            # short = time_range("2010-01-12 8:30:00", "2010-01-12 8:45:00", 2, 60)
+            # Result: [('2010-01-12 10:00:00', '2010-01-12 08:37:00'), ('2010-01-12 10:00:00', '2010-01-12 08:45:00')]
+            # Excepted: []
+            if low < high:
+                overlap_time.append((low, high))
     return overlap_time
 
+
+# move it to the `test_given_input()` function
 if __name__ == "__main__":
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
     print(compute_overlap_time(large, short))
+
+
+# test whether the bug was fixed
+# large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+# short = time_range("2010-01-12 08:30:00", "2010-01-12 8:45:00", 2, 60)
+# print(compute_overlap_time(large, short))
