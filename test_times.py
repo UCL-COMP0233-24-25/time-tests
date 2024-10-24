c16ef807fdc2032e9e1553d9d755d20b87ca9b06
shiying <shiying.wei@ucl.ac.uk>
2024-10-24 13:34:47 +0100

    add run condition

--- test_times.py
+++ test_times.py
@@ -7,3 +7,6 @@ def test_given_input():
         result = compute_overlap_time(large, short)
         expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
         assert result == expected
+
+if __name__ == "__main__":
+        test_given_input()
